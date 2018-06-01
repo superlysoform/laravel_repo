fb7d5c2c0ab9cc3e42a72ec2d94312ff37936487
superlysoform <nicsojeda@gmail.com>
2018-06-01 16:16:18 -0300

    'repaso'

---
+++ repaso/resources/views/index.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title></title>
+<style media="screen">
+.contenedor {
+display: flex;
+width: 500px;
+}
+
+.rawr {
+width: 50%;
+margin-top: 110px;
+}
+
+.dino {
+width: 50%;
+}
+</style>
+</head>
+<body>
+<div class="contenedor">
+
+<div class="rawr">
+<marquee>RAWR RAWRRRR</marquee>
+<marquee>RAWR RAWRRRR</marquee>
+</div>
+<div class="dino">
+<pre>
+
+
+                    ;,--""-.`\  ,=--':%:%:\
+                            /"       "| /-".:%%%%%%%\
+                                            ;,-"'`)%%)
+                                           /"      "|
+</pre>
+</div>
+</div>
+
+</body>
+</html>
